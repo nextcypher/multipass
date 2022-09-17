818d6ee65620aa06669adcbeca72ddf3a79f2838
Jeff Garzik <jeff@bloq.com>
2022-09-17 17:44:34 -0400

    gen-loot: minor cleanups

--- gen-loot.c
+++ gen-loot.c
@@ -6,9 +6,12 @@
 #include <stdio.h>
 #include <limits.h>
 
+#define ARRAY_SZ(array) (sizeof((array))/sizeof((array)[0]))
+
 static const unsigned int DEF_N_TOKENS = 1138;
 static const unsigned int NXC_SEED = 2716689190;
 
+// qsort helper function; compare two unsigned ints.
 static int cmp_int(const void *p1, const void *p2)
 {
 	const unsigned int *a = p1;
@@ -17,6 +20,7 @@ static int cmp_int(const void *p1, const void *p2)
 	return (*a) - (*b);
 }
 
+// roll(d6)
 static unsigned int roll_d6(void)
 {
 	unsigned int val = (random() % 6) + 1;
@@ -24,6 +28,7 @@ static unsigned int roll_d6(void)
 	return val;
 }
 
+// diagnostic: dump result array
 static void dump_res(const char *pfx, const unsigned int *res)
 {
 #if 0
@@ -32,10 +37,12 @@ static void dump_res(const char *pfx, const unsigned int *res)
 #endif
 }
 
+// roll 4d6, and drop the lowest score
 static unsigned int roll_4d6(void)
 {
 	unsigned int res[4];
 
+	// roll(4d6)
 	for (unsigned int n = 0; n < 4; n++)
 		res[n] = roll_d6();
 
@@ -43,6 +50,7 @@ static unsigned int roll_4d6(void)
 	qsort(res, 4, sizeof(unsigned int), cmp_int);
 	dump_res("post", res);
 
+	// only include greatest 3 in total
 	unsigned int total = 0;
 	for (unsigned int n = 1; n < 4; n++)
 		total += res[n];
@@ -51,8 +59,8 @@ static unsigned int roll_4d6(void)
 	return total;
 }
 
-#define N_ATTRIBS 6
-static const char *attribs[N_ATTRIBS] = {
+// the standard character attributes
+static const char *attribs[] = {
 	"str",
 	"dex",
 	"con",
@@ -61,6 +69,7 @@ static const char *attribs[N_ATTRIBS] = {
 	"chr",
 };
 
+// generate JSON metadata (the loot) for a single NFT
 static void generate_token_text(unsigned int id)
 {
 	char fn[PATH_MAX];
@@ -69,11 +78,11 @@ static void generate_token_text(unsigned int id)
 
 	fprintf(f, "{\n");
 
-	for (unsigned int n = 0; n < N_ATTRIBS; n++) {
+	for (unsigned int n = 0; n < ARRAY_SZ(attribs); n++) {
 		fprintf(f, "\t\"%s\":%u%s\n",
 			attribs[n],
 			roll_4d6(),
-			(n == (N_ATTRIBS-1)) ? "" : ",");
+			(n == (ARRAY_SZ(attribs)-1)) ? "" : ",");
 	}
 	fprintf(f, "}\n");
 
@@ -82,15 +91,19 @@ static void generate_token_text(unsigned int id)
 
 int main (int argc, char *argv[])
 {
+	// use seed from mkseed for determinism
 	srandom(NXC_SEED);
 
+	// single CLI arg: number of tokens
 	unsigned int n_tokens = DEF_N_TOKENS;
 	if ((argc == 2) && (atoi(argv[1]) > 0))
 		n_tokens = (unsigned int) atoi(argv[1]);
 
+	// generate, starting with number 1
 	for (unsigned int n = 0; n < n_tokens; n++)
 		generate_token_text(n + 1);
 
+	// success
 	return 0;
 }
 
